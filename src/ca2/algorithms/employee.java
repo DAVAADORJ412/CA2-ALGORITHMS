da3935f94c9588f5aa3c83fc3ed150248e2364ca
DAVAADORJ412 <davaadorjbayasgalan@gmail.com>
2025-11-26 23:59:55 +0000

    added employee class

--- src/ca2/algorithms/employee.java
+++ src/ca2/algorithms/employee.java
@@ -4,10 +4,23 @@
  */
 package ca2.algorithms;
 
-/**
- *
- * @author sarna
- */
 public class employee {
-    
+    public String name;
+    public String managerType;
+    public String department;
+
+    public employee(String name, String managerType, String department) {
+        this.name = name;
+        this.managerType = managerType;
+        this.department = department;
+    }
+
+    public String getName() { return name; }
+    public String getManagerType() { return managerType; }
+    public String getDepartment() { return department; }
+
+    @Override
+    public String toString() {
+        return name + " | " + managerType + " | " + department;
+    }
 }
