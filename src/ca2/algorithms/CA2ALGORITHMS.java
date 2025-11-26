3b0e51b2b13025c452c85a085c673f2b41632f84
DAVAADORJ412 <davaadorjbayasgalan@gmail.com>
2025-11-26 12:47:37 +0000

    added comment for main menu

--- src/ca2/algorithms/CA2ALGORITHMS.java
+++ src/ca2/algorithms/CA2ALGORITHMS.java
@@ -15,6 +15,7 @@ public class CA2ALGORITHMS {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Davaadorj");
     }
     
 }
