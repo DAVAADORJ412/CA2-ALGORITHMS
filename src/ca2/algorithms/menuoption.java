989cdbda76cedfa2d85d1823c8c1d1476fe76618
DAVAADORJ412 <davaadorjbayasgalan@gmail.com>
2025-11-27 00:21:54 +0000

    update menuoption class

--- src/ca2/algorithms/menuoption.java
+++ src/ca2/algorithms/menuoption.java
@@ -8,6 +8,27 @@ package ca2.algorithms;
  *
  * @author sarna
  */
-public class menuoption {
+public enum menuoption {
     
+    SORT(1, "Sort employees list"),
+    SEARCH(2, "Search employee by name"),
+    ADD_RECORD(3, "Add new employee"),
+    CREATE_TREE(4, "Build employee hierarchy"),
+    EXIT(5, "Exit program");
+
+    public final int code;
+    public final String text;
+    
+    menuoption(int code, String text) {
+        this.code = code;
+        this.text = text;
+}
+    public static menuoption fromCode(int code) {
+        for (menuoption m : values()) {
+            if (m.code == code) {
+                
+            }
+        }
+        return null;
+    }
 }
