2dbac561cedc3764d8da2eabcbcac79ea80fee72
DAVAADORJ412 <davaadorjbayasgalan@gmail.com>
2025-11-27 00:48:53 +0000

    updated

--- src/ca2/algorithms/CA2ALGORITHMS.java
+++ src/ca2/algorithms/CA2ALGORITHMS.java
@@ -4,18 +4,49 @@
  */
 package ca2.algorithms;
 
+import java.io.File;
+import java.util.*;
+
 /**
  *
  * @author sarna
  */
 public class CA2ALGORITHMS {
 
-    /**
-     * @param args the command line arguments
-     */
+    static Scanner input = new Scanner(System.in);
+    static List<employee> list = new ArrayList<>();
+    static treenode root;
+    
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Davaadorj");
+     
+        System.out.print("Enter CSV filename: ");
+        loadFile(input.nextLine());
+
+        while (true) {
+            System.out.println("\n============== MENU ==============");
+            System.out.println("1. Sort Employees");
+            System.out.println("2. Search Employee");
+            System.out.println("3. Add Record");
+            System.out.println("4. Create Binary Tree (20 employees)");
+            System.out.println("5. Exit");
+            System.out.print("Choose: ");
+
+            int c = Integer.parseInt(input.nextLine());
+            menuoption option = menuoption.fromCode(c);
+
+            if (option == null) {
+                System.out.println("Invalid");
+                continue;
+            }
+
+            switch (option) {
+                case SORT -> sort();
+                case SEARCH -> search();
+                case ADD_RECORD -> addRecord();
+                case CREATE_TREE -> createTree();
+                case EXIT -> { System.out.println("Goodbye"); return; }
+            }
+        }   
     }
     
 }
